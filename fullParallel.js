c0d0c122f0c90452c1bea624b398322591c214ce
raviguruiitr@gmail.com <raviguruiitr@gmail.com>
2016-05-16 16:12:27 +0530

    after testing

--- fullParallel.js
+++ fullParallel.js
@@ -1,4 +1,26 @@
-function(){
+function async(arg,callback){
+	console.log("function called for="+arg);
+	setTimeout(function(){callback(arg*2)},1000);
+};
 
-	
+var param=[1,2,3,4,5];
+var final=[];
+
+function finalFunction(){
+	console.log("final array =",final);
 }
+
+function parallel(){
+
+	param.forEach(function(item){
+		async(item,function(arg){
+			final.push(arg);
+			if(final.length==param.length)
+			{
+				finalFunction();
+			}
+		});
+	});
+
+};
+parallel();
